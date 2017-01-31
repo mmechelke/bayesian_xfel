c6827e39ea06c3fa3066d824553513b44d5bc18c
mmechelke <martin.mechelke@gmail.com>
2017-01-31 13:13:27 +0100

    Added C extension module

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
 from setuptools import setup, Extension
 import numpy
 
+xfelmodule = Extension('bxfel/_bxfel',
+                          include_dirs = [numpy.get_include(),
+                                       './bxfel/c/include'],
+                           sources = ['./bxfel/c/grid.c',
+                                      './bxfel/c/xfel.c'],
+                           extra_compile_args = ['-Wno-return-type',
+                                                 '-Wno-unused-variable',
+                                                 '-Wno-unused-function',
+                                                 '-Wno-unused-but-set-variable']
+)
+
 setup (name = 'bxfel', \
        version = '1.0', \
        packages  = ['bxfel',
                     'bxfel.core',
                     'bxfel.orientation',
-                    'bxfel.sampling',
-                    'bxfel.test',
+                    'bxfel.sampling'
                 ],
        package_dir = {'bxfel': './bxfel'},
        package_data={'bxfel':['orientation/resources/gauss/*.dat',
                               'orientation/resources/chebyshev/*.dat']},
        include_package_data=True,
        requires = ['numpy', 'scipy'],
-       zip_safe=False
+       zip_safe=False,
+       ext_modules = [xfelmodule,]
        
 )
 
