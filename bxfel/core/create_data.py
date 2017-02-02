77456a3835844f107dd2881a661f86c631ab6ba4
mmechelke <martin.mechelke@gmail.com>
2017-02-02 10:05:35 +0100

    Added module to naivly simulated scattering data

---
+++ bxfel/core/create_data.py
@@ -0,0 +1,98 @@
+import numpy as np
+
+from csbplus.statistics.rand import uniform_random_rotation
+
+from xfel.grid import Grid
+
+import scipy.ndimage
+
+class Experiment(object):
+
+    def __init__(self, density):
+        n = np.shape(density)[0]
+
+        coords = np.linspace(-1.,1.,n)
+        self._n = n
+        self._rho = Grid(coords ,coords,coords, density)
+
+    def generate_data(self, n_data):
+        raise NotImplementedError("Subclass responsability")
+
+
+class GaussianSlices(Experiment):
+
+
+    def __init__(self, density, scale, sigma, const_fluence=True, fluence_alpha=10., fluence_beta=10.):
+        super(GaussianSlices, self).__init__(density)
+        self._scale = float(scale)
+        self._sigma = float(sigma)
+
+        self._const_fluence = const_fluence
+        self._fluence_alpha = fluence_alpha
+        self._fluence_beta = fluence_beta
+
+    def generate_data(self, n_data, resolution=32):
+        """
+        @param n_data: number of images to generate_data
+        @param resolution: size of the grid
+        """
+        samples = []
+        Rs = uniform_random_rotation(n_data)
+        for R in Rs:
+            slice = self._rho.slice(R.T).T
+            slice = scipy.ndimage.zoom(slice,
+                                       float(resolution)/float(np.shape(slice)[0]))
+            scale = self._scale
+            if not self._const_fluence:
+                scale *= np.random.gamma(self._fluence_alpha, 1./self._fluence_beta)
+            noisy_slice = np.random.normal(scale * slice, self._sigma)
+            samples.append(noisy_slice)
+        return Rs, np.array(samples)
+
+class PoissonGaussianSlices(GaussianSlices):
+
+
+    def generate_data(self, n_data, resolution=32):
+        """
+        @param n_data: number of images to generate_data
+        @param resolution: size of the grid
+        """
+        samples = []
+        Rs = uniform_random_rotation(n_data)
+        for R in Rs:
+            slice = self._rho.slice(R.T).T
+            slice = scipy.ndimage.zoom(slice,
+                                       float(resolution)/float(np.shape(slice)[0]))
+            scale = self._scale
+            if not self._const_fluence:
+                scale *= np.random.gamma(self._fluence_alpha, 1./self._fluence_beta)
+            noisy_slice = np.random.normal(scale * slice, self._sigma * np.sqrt(scale* slice))
+            samples.append(noisy_slice)
+        return Rs, np.array(samples)
+
+
+
+class PoissonSlices(Experiment):
+
+
+    def __init__(self, density, scale = 1.):
+        super(PoissonSlices, self).__init__(density)
+        self._scale = scale
+
+    def generate_data(self, n_data, resolution=32):
+        """
+        @param n_data: number of images to generate_data
+        @param resolution: size of the grid
+        """
+        samples = []
+        Rs = uniform_random_rotation(n_data)
+        for R in Rs:
+            slice = self._rho.slice(R.T).T
+            slice = slice.clip(0.,1e300)
+            slice = scipy.ndimage.zoom(slice,
+                                       float(resolution)/float(np.shape(slice)[0]))
+            noisy_slice = np.random.poisson(self._scale * slice.clip(0.,1e100))
+            samples.append(noisy_slice)
+        return Rs, np.array(samples)
+
+
---
+++ test/test_generate_scattering_images.py
@@ -0,0 +1,61 @@
+import os
+
+import numpy as np
+import unittest
+import bxfel
+
+from bxfel.core.create_data import GaussianSlices, PoissonSlices
+
+from bxfel.io import mrc
+
+class TestPoissonSlices(unittest.TestCase):
+
+    def setUp(self):
+
+        data_path = os.path.dirname(bxfel.__file__)
+        phantom_path = os.path.join(data_path,
+                                    "data/volumes/phantom.mrc")
+
+        self.volume =  mrc.read(phantom_path)[0]
+        self.n = self.volume.shape[0]
+        self.generator = PoissonSlices(self.volume)
+        scale = 1e7
+
+    def test_generation(self):
+        n_samples = int(1e2)
+        resolution = self.volume.shape[0]
+        rs, data = self.generator.generate_data(n_samples, resolution)
+
+        self.assertEqual(rs.shape[0], n_samples)
+        self.assertEqual(rs.shape[1], 3)
+        self.assertEqual(rs.shape[2], 3)
+
+        self.assertEqual(data.shape[1], resolution)
+        self.assertEqual(data.shape[2], resolution)
+
+        for i in range(n_samples):
+            self.assertAlmostEqual(np.linalg.det(rs[i]),1.)
+        self.assertEqual(data.shape[0], n_samples)
+
+        n_samples = int(1e2)
+        resolution = np.random.randint(21,self.volume.shape[0])
+        rs, data = self.generator.generate_data(n_samples, resolution)
+
+        self.assertEqual(rs.shape[0], n_samples)
+        self.assertEqual(rs.shape[1], 3)
+        self.assertEqual(rs.shape[2], 3)
+
+        self.assertEqual(data.shape[1], resolution)
+        self.assertEqual(data.shape[2], resolution)
+        for i in range(n_samples):
+            self.assertAlmostEqual(np.linalg.det(rs[i]),1.)
+        self.assertEqual(data.shape[0], n_samples)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+
